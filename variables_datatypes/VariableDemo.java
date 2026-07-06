bec525b106939d0bca3e327b029f36066cfa999b
Aman Kumar <mr.ak.singh9771@gmail.com>
2026-07-07 05:12:05 +0530

    Added Variables and Data Types examples

---
+++ variables_datatypes/VariableDemo.java
@@ -0,0 +1,28 @@
+package variables_datatypes;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        // variable declaration
+        int marks;
+        // variable initialization
+        marks = 87;
+
+        int num = 45; // together declaration & initialization
+
+        // Different DataTypes
+
+        int age = 20;
+        double percentage = 82.45;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Aman";
+
+        // print
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Percentage:" + percentage);
+        System.out.println("Grade:" + grade);
+        System.out.println("Passed:" + isPassed);
+
+    }
+}
